afcfbecf1e7bcb92fd4e77e2387b2f50ad38d860
Sam van der Zwan <sam.vanderzwan@deltares.nl>
2022-01-13 17:20:41 +0100

    Removed override style sheet

--- conf.py
+++ conf.py
@@ -56,8 +56,3 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
-        ],
-    }
